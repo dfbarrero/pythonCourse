c665f01e9d847fe8f97d5608de37b269e647bfcb
David F. Barrero <david@aut.uah.es>
2018-02-09 16:15:17 +0100

    Introduction added

---
+++ 1-introduction/code/if.py
@@ -0,0 +1,6 @@
+temperature = float(input('What is the temperature? '))
+if temperature > 70:
+    print('Wear shorts.')
+else:
+    print('Wear long pants.')
+print('Get some exercise outside.')
---
+++ 1-introduction/code/operdemo.py
@@ -0,0 +1,7 @@
+a = int(input("Number: "))
+b = float(input("Number: "))
+c = (a * b) / 2
+c += 1
+d = c ** 2
+print("Result c: ", c)
+print("Result d: ", d)
