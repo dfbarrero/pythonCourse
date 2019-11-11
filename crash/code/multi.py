a55c379178054614c8085a56a2b44b393f4c1335
David F. Barrero <david.barrero@gmail.com>
2019-11-11 12:29:16 +0100

    New crash course slides added

---
+++ crash/code/multi.py
@@ -0,0 +1,15 @@
+table = 8
+start = 1
+max = 10
+s = '-' * 20
+print (s)
+print ('The table of 8')
+print (s)
+i = start
+while i <= max:
+    result = i * table
+    print (i, ' * ', table, ' =' , result)
+    i = i + 1
+print (s)
+print ('Done counting...')
+print (s)
